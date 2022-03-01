b4deb2ae51b061af07f6d020f2a159a35fe40a5a
albertdc1984 <92040589+albertdc1984@users.noreply.github.com>
2022-03-01 18:24:47 +0100

    Created pages modules

---
+++ pages/pokessr.js
@@ -0,0 +1,15 @@
+const PokemonSsr = ({ results }) => {
+  return (
+    <>
+      {results.map((pokemon) => (
+        <p key={pokemon.name}>{pokemon.name}</p>
+      ))}
+    </>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const res = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=10");
+  const { results } = await res.json();
+  return { props: { results } };
+};
